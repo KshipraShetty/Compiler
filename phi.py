a632df73cdd6095296a4bc354ab1a0c140eecc13
KshipraShetty <kshiprashetty@gmail.com>
2016-06-24 12:16:49 +0530

    Update phi.py

--- phi.py
+++ phi.py
@@ -13,19 +13,25 @@ for line in f1:
 		line1=line.strip()
 	dicti.update({int(line1):a})	
 print dicti
+dict=dicti.copy()
 f1.close()
+
+
 print("\n\n")
 f1=open('df.txt','r')
-
+dictio={}
 for line in f1:
 	line.strip()
 	lin=line.split(" ")
-	#i=0
 	for values in dicti[int(lin[0])]:
 		if values.find(" = ")!=-1:
 			lin1=values.split(" ")
 			obj="phi("+str(lin1[0])+","+str(lin1[0])+")"
-			dicti[int(lin[0])]=[obj]+dicti[int(lin[0])]
-			
-	
+			for i in range(1,len(lin)-1):
+				if obj not in dicti[int(lin[i])]:				
+					lin[i].strip()
+				
+					dicti[int(lin[i])]=[obj]+dicti[int(lin[i])]
+		else:
+			dicti[int(lin[0])]=(dicti[int(lin[0])])
 print dicti
